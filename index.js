57307e5c3a3e67b0a46df54504134f5546d03f21
shingchan95 <chanhei888@gmail.com>
2022-05-22 20:57:12 +0100

    update

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const Manager = require("./lib/Manager")
 const Intern = require("./lib/Intern")
 const Engineer = require("./lib/Engineer")
 
+
 let Manager_profile;
 let intern_profile;
 let Engineer_profile;
+
+
 
 var inquirer = require('inquirer');
      inquirer
@@ -59,7 +62,7 @@ var inquirer = require('inquirer');
          .then ((answers) => {
             Manager_profile = new Manager(employee_answer.employee_name, employee_answer.employee_id, employee_answer.employee_email, answers.employee_officeNumber)
             console.log(Manager_profile)
-            htmlrender()
+            managerrender()
         })
         
     }
@@ -97,26 +100,59 @@ var inquirer = require('inquirer');
     }
      
 
-    const htmlrender = () => {
-        const dataer = `<div class="team">
+
+    const managerrender = () => {
+        const {employee_name, id, email, officeNumber}=Manager_profile
+
+        let html_data = `<div class="team">
         <div class="card">
         <div class="card-header">
-            <h2>${Manager_profile.employee_name}</h2>
-            <p>Employee ID: ${Manager_profile.employee_id}</p>
-            <p>Email: <a href="mailto:${Manager_profile.employee_email}">${Manager_profile.employee_email}</a></p>
-            <p>Office Number: ${Manager_profile.employee_officeNumber}</p>
+        <h2>${employee_name}</h2>
+        <p>Employee ID: ${id}</p>
+        <p>Email: <a href="mailto:${email}">${email}</a></p>
+        <p>Office Number: ${officeNumber}</p>
         </div>
-        </div>`
-
-        // const fs = require('fs');
-        // fs.appendFile("./Develop/dist/team.html", data, (error) =>
-        // error ? console.error(error) : console.log("file has been created")
-        // );
+        </div>
+        <!--managers-->`
+        
 
         const fs = require('fs');
-        fs.readFile("./Develop/dist/team.html",{encoding:'utf8', flag:'r'}, (error, data) =>
-        error ? console.error(error) : console.log(data)
-        );
+        fs.readFile("./Develop/dist/team.html",{encoding:'utf8', flag:'r'}, (err, data) =>{
+            if (err) {
+                return console.log(err);
+            }
+            var myHtml = data.replace("<!--managers-->", html_data);
+ 
+                fs.writeFile("./Develop/dist/team.html", myHtml, 'utf8', function (err) {
+                    if (err) return console.log(err);
+                });
+            });   
+}
 
 
-    }
+    // <div class="team">
+    //         <div class="card">
+    //         <div class="card-header">
+    //             <h2>Manager</h2>
+    //             <p>Employee ID: 1</p>
+    //             <p>Email: <a href="mailto:manager@example.com">manager@example.com</a></p>
+    //             <p>Office Number: 01234 567890</p>
+    //         </div>
+    //     </div>
+    //         <div class="card">
+    //         <div class="card-header">
+    //             <h2>Engineer</h2>
+    //             <p>Employee ID: 2</p>
+    //             <p>Email: <a href="mailto:engineer@example.com">engineer@example.com</a></p>
+    //             <p>GitHub: <a href="https://github.com/engineer" target="_blank">engineer</a></p>
+    //         </div>
+    //     </div>
+    //         <div class="card">
+    //         <div class="card-header">
+    //             <h2>Intern</h2>
+    //             <p>Employee ID: 3</p>
+    //             <p>Email: <a href="mailto:intern@example.com">intern@example.com</a></p>
+    //             <p>School: Intern School</p>
+    //         </div>
+    //     </div>
+    //     </div>  
